612c54c1191670d70a864ec1efe859602e208990
Florian.zl <f.zitlau@web.de>
2021-04-16 16:50:59 +0200

    fixed the algorithm. only xPos in keyCompare() does not has the value it should

--- Optimierer.java
+++ Optimierer.java
@@ -2,17 +2,14 @@ import basis.*;
 
 import java.awt.*;
 import java.util.*;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 
 public class Optimierer {
 
-    private Bild bild;
-    private Stift stift;
-    private int coordsCounter = 0;
-    private int[] coords = {65, 245};
+    private Bild bild, bildorg, bild1;
+    private Stift stift, stift1;
+    private int coordsCounter;
+    private int[] coords = new int[6];
     public BeschriftungsFeld bestLabel;
     public Map<Integer, Map.Entry<Integer, Integer>> leftWay = new HashMap<>();
     public Map<Integer, Map.Entry<Integer, Integer>> rightWay = new HashMap<>();
@@ -21,11 +18,10 @@ public class Optimierer {
 
 
     public Optimierer() {
-
+        coordsCounter = 0;
         bestLabel = new BeschriftungsFeld("Starten um schnellsten Geher zu finden!",190,130,320,30);
         bestLabel.setzeSchriftGroesse(12);
     }
-
 
     public void best(int leftmax, int rightmax, int fLeftMax, int fRightMax) {
         //compares the 4 methods and finds out which is the best
@@ -55,164 +51,198 @@ public class Optimierer {
     }
 
 
-    private void right(int start) {
-        //this method looks if on the right side of the current pos is a green field
-        int xStart = leftWay.get(start).getKey();
-        //yPos is equal to yStart so it's the same
-        int yPos = leftWay.get(start).getValue();
-        int xPos = xStart;
-
-        //it stops if hitting a black field
-        while (!bild.farbeVon(xPos, yPos).equals(Color.BLACK)){
-
-            //if hitting a Green field add the pos to coords
-            if (bild.farbeVon(xPos, yPos).equals(Color.GREEN)){
-                coords[coordsCounter] = xPos;
-                coordsCounter++;
-                coords[coordsCounter] = yPos;
-                coordsCounter++;
-            }
-            xPos = xPos + 10;
-        }
-
-    }
-
-    private void left(int start) {
-        //this method looks if on the right side of the current pos is a green field
-        int xStart = leftWay.get(start).getKey();
-        //yPos is equal to yStart so it's the same
-        int yPos = leftWay.get(start).getValue();
-        int xPos = xStart;
-
-        //it stops if hitting a black field
-        while (!bild.farbeVon(xPos, yPos).equals(Color.BLACK)) {
-
-            //if hitting a Green field add the pos to coords
-            if (bild.farbeVon(xPos, yPos).equals(Color.GREEN)) {
-                coords[coordsCounter] = xPos;
-                coordsCounter++;
-                coords[coordsCounter] = yPos;
-                coordsCounter++;
-            }
-            xPos = xPos - 10;
-        }
-    }
-
-    private void up(int start) {
-        //this method looks if on the right side of the current pos is a green field
-        int yStart = leftWay.get(start).getValue();
-        //xPos is equal to yStart so it's the same
-        int xPos = leftWay.get(start).getKey();
-        int yPos = yStart;
-
-        //it stops if hitting a black field
-        while (!bild.farbeVon(xPos, yPos).equals(Color.BLACK)){
-
-            //if hitting a Green field add the pos to coords
-            if (bild.farbeVon(xPos, yPos).equals(Color.GREEN)){
-                coords[coordsCounter] = xPos;
-                coordsCounter++;
-                coords[coordsCounter] = yPos;
-                coordsCounter++;
-            }
-            yPos = yPos - 10;
-        }
-    }
-
-    private void down(int start) {
-        //this method looks if on the right side of the current pos is a green field
-        int yStart = leftWay.get(start).getValue();
-        //yPos is equal to yStart so it's the same
-        int xPos = leftWay.get(start).getKey();
-        int yPos = yStart;
-
-        //it stops if hitting a black field
-        while (!bild.farbeVon(xPos, yPos).equals(Color.BLACK)) {
-
-            //if hitting a Green field add the pos to coords
-            if (bild.farbeVon(xPos, yPos).equals(Color.GREEN)){
-                coords[coordsCounter] = xPos;
-                coordsCounter++;
-                coords[coordsCounter] = yPos;
-                coordsCounter++;
-            }
-            yPos = yPos + 10;
-        }
-    }
-
-
-    public int keyCompare(int key1) {
-        //todo fixing this method
-        //checks if the current pos or the next green field in coords array has a higher step number
+    private int keyCompare(int key1, int x2, int y2){
+        System.out.println("in KeyCompare");
         int key2 = 0;
+        System.out.println("x2 = " + x2 + " y2 = " + y2);
+        for (Map.Entry<Integer, Map.Entry<Integer, Integer>> map : leftWay.entrySet()) {
 
-        //
-        do {
-            //gets the key of the pos of green fields
-            for (Map.Entry<Integer, Map.Entry<Integer, Integer>> map : leftWay.entrySet()) {
+            if (map.getValue().getKey() == x2 && map.getValue().getValue() == y2){
 
-                if (map.getValue().getKey() == coords[0] && map.getValue().getValue() == coords[1]){
-
-                    key2 = map.getKey();
-                }
+                key2 = map.getKey();
+                System.out.println("key2 = " + key2);
             }
-
-            //removes the first two indices of coords
-            List<Integer> arrayList = IntStream.of(coords).boxed().collect(Collectors.toList());
-            System.out.println(coords.length);
-            arrayList.remove(1);
-            arrayList.remove(0);
-            coords = arrayList.stream().mapToInt(Integer::intValue).toArray();
-            System.out.println("coords length: " + coords.length);
-
-        }while (coords.length >= 2);
-
-        if (key1 < key2) {
-            return key2;
         }
-        else return key1;
+        if (key1 < key2) return key2;
+        return key1;
     }
 
-    private void bewege(int x1, int x2, int y1, int y2){
-        stift = new Stift();
-        stift.maleAuf(bild);
+    /*
+    private void move(int x1, int x2, int y1, int y2){
         int tempX = 0;
         int tempY = 0;
+
         if (x1 < x2) tempX = x2 - x1;
         else tempX = x1 - x2;
         if (y1 < y2) tempY = y2 - y1;
         else tempY = y1 - y2;
-
         int distance = tempX + tempY;
+        System.out.println(distance);
 
         for (int i = 0; i < distance / 10; i++){
-            stift.dreheInRichtung(x2, y2);
-            stift.setzeFarbe(Color.PINK);
-            stift.hoch();
-            stift.bewegeUm(10);
-            stift.runter();
-            stift.setzeFuellMuster(1);
-            stift.zeichneKreis(4);
-            stift.hoch();
+            stift1.dreheInRichtung(x2, y2);
+            stift1.setzeFarbe(Color.PINK);
+            stift1.hoch();
+            stift1.bewegeUm(10);
+            stift1.runter();
+            stift1.setzeFuellMuster(1);
+            stift1.zeichneKreis(4);
+            stift1.hoch();
             Hilfe.warte(10);
         }
     }
+    */
 
+    private int right(int i) {
+        int xPos = leftWay.get(i).getKey() + 10;
+        int yPos = leftWay.get(i).getValue();
+        System.out.println("xPos = " + xPos);
 
-    public void leftOptimize() {
-        //geher.start(65, 245);
-        bild = new Bild();
-        bild.ladeBild("D:\\projects\\IntelliJ_Template\\Mazes\\vickymaze.png");
-        bild.setzePosition(200,200);
-        /*
-        int startKey = keyCompare(1);
-        right(startKey);
-        left(startKey);
-        up(startKey);
-        down(startKey);
-        bewege(leftWay.get(startKey).getKey(), leftWay.get(keyCompare(startKey)).getKey(), leftWay.get(startKey).getValue(), leftWay.get(keyCompare(startKey)).getValue());
-        bewege(65, 65, 235, 225);
-        */
+        while (!bild1.farbeVon(xPos, yPos).equals(Color.BLACK)){
+
+            if (bild1.farbeVon(xPos, yPos).equals(Color.PINK)){
+                System.out.println("GOT PINK! right");
+                return keyCompare(i, xPos, yPos);
+            }
+            xPos = xPos + 10;
+        }
+        return i;
+    }
+
+    private int left(int i) {
+        int xPos = leftWay.get(i).getKey();
+        int yPos = leftWay.get(i).getValue();
+
+        while (!bild1.farbeVon(xPos, yPos).equals(Color.BLACK)){
+            if (bild1.farbeVon(xPos, yPos).equals(Color.PINK)){
+                return keyCompare(i, xPos, yPos);
+            }
+            else{
+                xPos = xPos - 10;
+            }
+        }
+        return i;
+    }
+
+    private int up(int i) {
+        int xPos = leftWay.get(i).getKey();
+        int yPos = leftWay.get(i).getValue();
+
+        while (!bild1.farbeVon(xPos, yPos).equals(Color.BLACK)){
+            if (bild1.farbeVon(xPos, yPos).equals(Color.PINK)){
+                return keyCompare(i, xPos, yPos);
+            }
+            else{
+                yPos = yPos - 10;
+            }
+        }
+        return i;
+    }
+
+    private int down(int i) {
+        int xPos = leftWay.get(i).getKey();
+        int yPos = leftWay.get(i).getValue();
+
+        while (!bild1.farbeVon(xPos, yPos).equals(Color.BLACK)){
+            if (bild1.farbeVon(xPos, yPos).equals(Color.PINK)){
+                return keyCompare(i, xPos, yPos);
+            }
+            else{
+                yPos = yPos + 10;
+            }
+        }
+        return i;
+    }
+
+    public void leftOptimize(Stift stift, Bild bild) {
+        String richtung = "";
+        int i = 1;
+        stift1 = stift;
+        bild1 = bild;
+        stift1.maleAuf(bild1);
+        stift1.bewegeAuf(65, 245);
+        stift1.dreheBis(90);
+        stift1.setzeFarbe(Color.PINK);
+        stift1.setzeFuellMuster(1);
+        stift1.hoch();
+
+        while (i <= leftWay.size()) {
+
+            if(i < leftWay.size()) {
+                if (leftWay.get(i + 1).getKey() > leftWay.get(i).getKey()) richtung = "rechts";
+                else if (leftWay.get(i + 1).getKey() < leftWay.get(i).getKey()) richtung = "links";
+                else if (leftWay.get(i + 1).getValue() < leftWay.get(i).getValue()) richtung = "oben";
+                else if (leftWay.get(i + 1).getValue() > leftWay.get(i).getValue()) richtung = "unten";
+            }
+
+            System.out.println(richtung);
+            System.out.println("i = " + i);
+            if (richtung == "rechts"){
+                if (left(i) > right(i) && left(i) > up(i) && left(i) > down(i)){
+                    stift1.bewegeAuf(leftWay.get(left(i)).getKey(), leftWay.get(left(i)).getValue());
+                }
+                else if (up(i) > right(i) && up(i) > left(i) && left(i) > down(i)){
+                    stift1.bewegeAuf(leftWay.get(up(i)).getKey(), leftWay.get(up(i)).getValue());
+                }
+                else if (down(i) > right(i) && left(i) > up(i) && left(i) > left(i)){
+                    stift1.bewegeAuf(leftWay.get(down(i)).getKey(), leftWay.get(down(i)).getValue());
+                }
+                else stift1.bewegeAuf(leftWay.get(i).getKey(), leftWay.get(i).getValue());
+            }
+            if (richtung == "links"){
+
+                if(right(i) > left(i) && right(i) > up(i) && right(i) > down(i)){
+                    stift1.bewegeAuf(leftWay.get(right(i)).getKey(), leftWay.get(right(i)).getValue());
+                }
+                else if (up(i) > right(i) && up(i) > left(i) && left(i) > down(i)){
+                    stift1.bewegeAuf(leftWay.get(up(i)).getKey(), leftWay.get(up(i)).getValue());
+                }
+                else if (down(i) > right(i) && left(i) > up(i) && left(i) > left(i)){
+                    stift1.bewegeAuf(leftWay.get(down(i)).getKey(), leftWay.get(down(i)).getValue());
+                }
+                else stift1.bewegeAuf(leftWay.get(i).getKey(), leftWay.get(i).getValue());
+            }
+            if (richtung == "oben"){
+                if(right(i) > left(i) && right(i) > up(i) && right(i) > down(i)){
+                    System.out.println("right ist am größten");
+                    stift1.bewegeAuf(leftWay.get(right(i)).getKey(), leftWay.get(right(i)).getValue());
+                }
+                else if (left(i) > right(i) && left(i) > up(i) && left(i) > down(i)){
+                    System.out.println("left ist am größten");
+                    stift1.bewegeAuf(leftWay.get(left(i)).getKey(), leftWay.get(left(i)).getValue());
+                }
+                else if (down(i) > right(i) && left(i) > up(i) && left(i) > left(i)){
+                    System.out.println("down ist am größten");
+                    stift1.bewegeAuf(leftWay.get(down(i)).getKey(), leftWay.get(down(i)).getValue());
+                }
+                else stift1.bewegeAuf(leftWay.get(i).getKey(), leftWay.get(i).getValue());
+                System.out.println("right = " + right(i));
+                System.out.println("left = " + left(i));
+                System.out.println("up = " + up(i));
+                System.out.println("down = " + down(i));
+
+            }
+            if (richtung == "unten"){
+
+                if(right(i) > left(i) && right(i) > up(i) && right(i) > down(i)){
+                    stift1.bewegeAuf(leftWay.get(right(i)).getKey(), leftWay.get(right(i)).getValue());
+                }
+                else if (left(i) > right(i) && left(i) > up(i) && left(i) > down(i)){
+                    stift1.bewegeAuf(leftWay.get(left(i)).getKey(), leftWay.get(left(i)).getValue());
+                }
+                else if (up(i) > right(i) && up(i) > left(i) && left(i) > down(i)){
+                    stift1.bewegeAuf(leftWay.get(up(i)).getKey(), leftWay.get(up(i)).getValue());
+                }
+                else stift1.bewegeAuf(leftWay.get(i).getKey(), leftWay.get(i).getValue());
+            }
+            System.out.println("=========================");
+            //stift1.bewegeAuf(leftWay.get(i).getKey(), leftWay.get(i).getValue());
+            stift1.runter();
+            stift1.zeichneKreis(4);
+            stift1.hoch();
+            Hilfe.warte(10);
+            i++;
+        }
     }
 
 /*
